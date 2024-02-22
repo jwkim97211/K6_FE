5f4a4ae498b28e05247ce79c511926717ce0d9b3
jwkim97211 <lkwe998@naver.com>
2024-02-22 13:01:19 +0900

    0222

--- 03/03.js
+++ 03/03.js
@@ -1,8 +1,14 @@
-function handleClick(n) {
-    // 메세지 영역 가져오기1
-    // document.getElementById("msgArea").innerHTML = "안녕하세요";
+// 함수작성1
+//function handleClick(n) {
+// 메세지 영역 가져오기1
+// document.getElementById("msgArea").innerHTML = "안녕하세요";
 
-    // 메세지 영역 가져오기2
-    //document.querySelector("#msgArea").innerHTML = `<h2>버튼${n}이 눌러졌습니다.</h2>`;
-    document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다.</h2>";
+// 메세지 영역 가져오기2
+//document.querySelector("#msgArea").innerHTML = `<h2>버튼${n}이 눌러졌습니다.</h2>`;
+//document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다.</h2>";
+//}
+
+// 함수작성2 : 화살표함수
+const handleClick = (n) => {
+    document.querySelector("#msgArea").innerHTML = `<h2>버튼${n}이 눌러졌습니다.</h2>`;
 }

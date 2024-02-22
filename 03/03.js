bc61c779c817a0a46c678cb9a36c10b55b07f16a
jwkim97211 <lkwe998@naver.com>
2024-02-22 12:53:08 +0900

    0222

---
+++ 03/03.js
@@ -0,0 +1,8 @@
+function handleClick(n) {
+    // 메세지 영역 가져오기1
+    // document.getElementById("msgArea").innerHTML = "안녕하세요";
+
+    // 메세지 영역 가져오기2
+    //document.querySelector("#msgArea").innerHTML = `<h2>버튼${n}이 눌러졌습니다.</h2>`;
+    document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다.</h2>";
+}

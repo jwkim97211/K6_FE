31a69b6421f14009ef9a450bf6f2422ae449c33a
jwkim97211 <lkwe998@naver.com>
2024-02-26 17:43:03 +0900

    0226

--- 07/07.js
+++ 07/07.js
@@ -1,18 +1,21 @@
-const handleChange = (s1,s2,lb1,lb2)=> {
-    if(s1.value=="℃") {
+const handleChange = (s1, s2, lb1, lb2,t1,t2) => {
+    if (s1.value == "℃") {
         s2.value = "℉";
-        lb1.innerHTML="℃";
-        lb2.innerHTML="℉";
+        lb1.innerHTML = "℃";
+        lb2.innerHTML = "℉";
     }
-    else  {
-        s2.value = "℃" ;
-        lb1.innerHTML="℉";
-        lb2.innerHTML="℃";
-}
+    else {
+        s2.value = "℃";
+        lb1.innerHTML = "℉";
+        lb2.innerHTML = "℃";
+    }
+    t1.value="";
+    t1.focus();
+    t2.value="";
 
 }
 
-document.addEventListener("DOMContentLoaded",()=> {
+document.addEventListener("DOMContentLoaded", () => {
     const sel1 = document.querySelector("#sel1");
     const sel2 = document.querySelector("#sel2");
 
@@ -20,25 +23,22 @@ document.addEventListener("DOMContentLoaded",()=> {
     const txt2 = document.querySelector("#txt2");
 
     const labels = document.querySelectorAll("label")
-    
-    sel1.addEventListener("change", ()=>{
-        console.log(sel1.value);
-        handleChange(sel1,sel2, labels[0],labels[1]);
+
+    sel1.addEventListener("change", () => {
+        handleChange(sel1, sel2, labels[0], labels[1],txt1,txt2);
     });
 
-    sel2.addEventListener("change", ()=>{
-        console.log(sel2.value);
-        handleChange(sel2,sel1,labels[1],labels[0]);
+    sel2.addEventListener("change", () => {
+        handleChange(sel2, sel1, labels[1], labels[0],txt1,txt2);
     });
 
+    txt1.addEventListener("input", () => {
+        if (sel1.value === "℃") {
+            txt2.value = (parseFloat(txt1.value) *(9*5)) + 32;
+        } else {
+            txt2.value = (parseFloat(txt1.value) - 32) *(5/9);
+        }
 
-
-
-
-
-
-
-
-
+    });
 
 });
